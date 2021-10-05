5a9a043c40748aa981bbf7dd6ddfe46afa205772
Mikita Kukavenka <godinfrog@gmail.com>
2021-10-05 15:59:57 +0300

    Обновил правила

---
+++ docs/neutral.md
@@ -0,0 +1,67 @@
+---
+sidebar_position: 3
+---
+
+# Нейтральные банды
+
+| 2d6 | Warband                                                   |
+| --- | --------------------------------------------------------- |
+| 2   | [(Dwarf) Dwarf Treasure Hunters](#dwarf-treasure-hunters) |
+| 3   | (Dwarf) Guild of Disgraced Engineers                      |
+| 4   | (High Elf) High Elves                                     |
+| 5   | (High Elf) Shadow Warriors                                |
+| 6   | (Human) Reiklanders                                       |
+| 7   | (Human) Sorcerous Society                                 |
+| 8   | (Human) The Outlaws of Stirwood Forest                    |
+| 9   | (Orcs & Goblins) Orc Mob                                  |
+| 10  | (Skaven) Skaven                                           |
+| 11  | (Skaven/Goblin) Underworld Alliance                       |
+| 12  | (Undead) Undead                                           |
+
+## Dwarf Treasure Hunters
+
+### Обычная версия
+
+- **Dwarf Noble** (leader): Dwarf axe, Light Armour, Shield
+- **Dwarf Engineer** (Expert Weaponsmith): Sword, Pistol (6'';s4)
+- **Dwarf Troll Slayer** (Deathwish): Dwarf axe, Dwarf axe
+- **Dwarf Thunderer**: Handgun (24'';s4)
+- **Dwarf Thunderer**: Handgun (24'';s4)
+- **Dwarf Thunderer**: Handgun (24'';s4)
+
+*Dwarf axe*: Cutting Edge, Parry
+
+**Expert Weaponsmith**: A Dwarf Engineer is a master of mechanical devices. By using stronger construction materials and time-tested secrets of Dwarf engineering, a Dwarf Engineer can increase the distance the warband’s missile weapons can shoot. All Dwarf missile weapons in the warband have their range increased by 3" for Pistols and 6" for Crossbows and Handguns. Any range increases are only maintained as long as the Dwarf Engineer remains with the warband.
+
+**Deathwish**: Troll Slayers seek an honourable death in combat. They are completely immune to all psychology and never need to test if fighting alone.
+
+| name                   | **M** | **WS** | **BS** | **S** | **T** | **W** | **I** | **A** | **Ld** |
+| ---------------------- | ----- | ------ | ------ | ----- | ----- | ----- | ----- | ----- | ------ |
+| **Dwarf Noble**        | 3     | 5      | 4      | 3     | 4     | 1     | 2     | 1     | 9      |
+| **Dwarf Engineer**     | 3     | 4      | 3      | 3     | 4     | 1     | 2     | 1     | 9      |
+| **Dwarf Troll Slayer** | 3     | 4      | 3      | 3     | 4     | 1     | 2     | 1     | 9      |
+| **Dwarf Thunderer**    | 3     | 4      | 3      | 3     | 4     | 1     | 2     | 1     | 9      |
+
+### Сложная версия
+
+- **Dwarf Noble** (leader): Dwarf axe, Gromril Armour, Shield, Helmet; +1 W
+- **Dwarf Engineer** (Expert Weaponsmith): Sword, Pistol brace (6'';s4), Light armour
+- **Dwarf Troll Slayer** (Deathwish): Dwarf axe, Dwarf axe; +1 A, +1 T
+- **Dwarf Thunderer**: Handgun (24'';s4)
+- **Dwarf Thunderer**: Handgun (24'';s4)
+- **Dwarf Thunderer**: Handgun (24'';s4)
+- **Beardling**: Dwarf axe, Light Armour
+
+*Dwarf axe*: Cutting Edge, Parry
+
+**Expert Weaponsmith**: A Dwarf Engineer is a master of mechanical devices. By using stronger construction materials and time-tested secrets of Dwarf engineering, a Dwarf Engineer can increase the distance the warband’s missile weapons can shoot. All Dwarf missile weapons in the warband have their range increased by 3" for Pistols and 6" for Crossbows and Handguns. Any range increases are only maintained as long as the Dwarf Engineer remains with the warband.
+
+**Deathwish**: Troll Slayers seek an honourable death in combat. They are completely immune to all psychology and never need to test if fighting alone.
+
+| name                   | **M** | **WS** | **BS** | **S** | **T** | **W** | **I** | **A** | **Ld** |
+| ---------------------- | ----- | ------ | ------ | ----- | ----- | ----- | ----- | ----- | ------ |
+| **Dwarf Noble**        | 3     | 5      | 4      | 3     | 4     | 2     | 2     | 1     | 9      |
+| **Dwarf Engineer**     | 3     | 4      | 3      | 3     | 4     | 1     | 2     | 1     | 9      |
+| **Dwarf Troll Slayer** | 3     | 4      | 3      | 3     | 5     | 1     | 2     | 2     | 9      |
+| **Dwarf Thunderer**    | 3     | 4      | 3      | 3     | 4     | 1     | 2     | 1     | 9      |
+| **Beardling**          | 3     | 3      | 2      | 3     | 4     | 1     | 2     | 1     | 8      |
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,114 +1,93 @@
-const lightCodeTheme = require('prism-react-renderer/themes/github');
-const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
-(module.exports = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+(
+  module.exports = {
+    title: "Mordheim Assault",
+    tagline: "Mordheim, the City of the Damned",
+    url: "https://mordheim-assault.owlbeardm.com",
+    baseUrl: "/",
+    onBrokenLinks: "throw",
+    onBrokenMarkdownLinks: "warn",
+    favicon: "img/logo.png",
+    organizationName: "c7d5a6", // Usually your GitHub org/user name.
+    projectName: "mordheim-assault", // Usually your repo name.
 
-  presets: [
-    [
-      '@docusaurus/preset-classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
+    presets: [
+      [
+        "@docusaurus/preset-classic",
+        /** @type {import('@docusaurus/preset-classic').Options} */
+        ({
+          docs: {
+            sidebarPath: require.resolve("./sidebars.js"),
+            // Please change this to your repo.
+            editUrl:
+              "https://github.com/facebook/docusaurus/edit/main/website/",
+          },
+          theme: {
+            customCss: require.resolve("./src/css/custom.css"),
+          },
+        }),
+      ],
+    ],
+
+    themeConfig:
+      /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
       ({
-        docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/edit/main/website/',
+        navbar: {
+          title: "Mordheim Assault",
+          logo: {
+            alt: "Mordheim Assault",
+            src: "img/mordheim_intro_logo.gif",
+          },
+          items: [
+            {
+              type: "doc",
+              docId: "intro",
+              position: "left",
+              label: "Tutorial",
+            },
+          ],
         },
-        blog: {
-          showReadingTime: true,
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/main/website/blog/',
+        footer: {
+          style: "light",
+          links: [
+            {
+              title: "Docs",
+              items: [
+                {
+                  label: "Campain",
+                  to: "/docs/intro",
+                },
+              ],
+            },
+            {
+              title: "Community",
+              items: [
+                {
+                  label: "Broheim",
+                  href: "http://broheim.net/",
+                }
+              ],
+            },
+            {
+              title: "More",
+              items: [
+                {
+                  label: "GitHub",
+                  href: "https://github.com/c7d5a6/mordheim-assault",
+                },
+              ],
+            },
+          ],
+          copyright: `${new Date().getFullYear()} @c7d5a6.`,
         },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+        prism: {
+          theme: lightCodeTheme,
+          // darkTheme: darkCodeTheme,
         },
       }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        title: 'My Site',
-        logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
-        },
-        items: [
-          {
-            type: 'doc',
-            docId: 'intro',
-            position: 'left',
-            label: 'Tutorial',
-          },
-          {to: '/blog', label: 'Blog', position: 'left'},
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Tutorial',
-                to: '/docs/intro',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'Blog',
-                to: '/blog',
-              },
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
-});
+  }
+);
